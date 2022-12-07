985f054acf3f715a9382921705d350d92562d56a
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-08 00:16:24 +0200

    Update fetchAll.js

--- src/redux/contacts/fetchAll.js
+++ src/redux/contacts/fetchAll.js
@@ -3,11 +3,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://6390e6c10bf398c73a963e5b.mockapi.io/api/v1';
 
-export const fetchAll = createAsyncThunk('tasks/fetchAll', async (_, thunk) => {
+export const fetchAll = createAsyncThunk('tasks/fetchAll', async (_, thunkAPI) => {
   try {
     const response = await axios.get('/contacts');
     return response.data;
   } catch (error) {
-    thunk.rejectWithValue(error.message);
+    thunkAPI.rejectWithValue(error.message);
   }
 });
