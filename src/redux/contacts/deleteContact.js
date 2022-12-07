6ac471632a93f8d2dc7555f9d6627924d78eb448
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-08 00:16:20 +0200

    Update deleteContact.js

--- src/redux/contacts/deleteContact.js
+++ src/redux/contacts/deleteContact.js
@@ -5,12 +5,12 @@ axios.defaults.baseURL = 'https://6390e6c10bf398c73a963e5b.mockapi.io/api/v1';
 
 export const deleteContact = createAsyncThunk(
   'tasks/deleteContact',
-  async (id, thunk) => {
+  async (id, thunkAPI) => {
     try {
       const response = await axios.delete(`/contacts/${id}`);
       return response.data;
     } catch (error) {
-      thunk.rejectWithValue(error.message);
+      thunkAPI.rejectWithValue(error.message);
     }
   }
 );
