6644ff9f5364fd400b792fdb502360795a8255df
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-08 00:16:14 +0200

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,18 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import persistReducer from 'redux-persist/es/persistReducer';
-// import storage from 'redux-persist/lib/storage';
 import { fetchAll } from './fetchAll';
 import { deleteContact } from './deleteContact';
 import { addContact } from './addContact';
 
-// const initialState = {
-//   value: [
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   ],
-// };
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -66,17 +56,7 @@ const contactsSlice = createSlice({
   },
 });
 
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-// };
-
-// export const contactsReducer = persistReducer(
-//   persistConfig,
-//   contactsSlice.reducer
-// );
 
 export const contactsReducer = contactsSlice.reducer;
 
 
-// export const { addContact, removeContact } = contactsSlice.actions;
