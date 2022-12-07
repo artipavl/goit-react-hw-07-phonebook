6e89f250817ac2d2e7d55e76298e261ff0a45671
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-08 00:35:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,22 @@ import { Section } from 'components/Section/Section';
 import { Phonebook } from 'components/Phonebook/Phonebook';
 import { ContactList } from 'components/Contacts/ContactList';
 import { Filter } from 'components/Filter/Filter';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchAll } from 'redux/contacts/fetchAll';
 
 export function App() {
+  const error = useSelector(state => state.contacts.error);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAll());
   }, [dispatch]);
 
+  useEffect(() => {
+    error && alert(error)
+  }, [error]);
+  
   return (
     <div>
       <Section title="Phonebook">
